cce8099ee715d9f0b0a03868487e28b3a78b1b46
Chinmay-Mahagaonkar <62831524+yamnihc04@users.noreply.github.com>
2022-04-10 14:21:36 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -154,6 +154,40 @@
       'share': false
     });
   });
+  
+  (function($) {
+    "use strict";
+  
+    // Back to top button
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+      } else {
+        $('.back-to-top').fadeOut('slow');
+      }
+    });
+    $('.back-to-top').click(function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 1500, 'easeInOutExpo');
+      return false;
+    });
+  
+    // Header fixed on scroll
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('#header').addClass('header-scrolled');
+      } else {
+        $('#header').removeClass('header-scrolled');
+      }
+    });
+  
+    if ($(window).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    }
+  
+    // Initialize Venobox
+    $(window).on('load
 
   // Portfolio details carousel
   $(".portfolio-details-carousel").owlCarousel({
